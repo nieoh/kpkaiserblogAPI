d704844df34f158d59f8118e4bb318e92fcbecf6
nieoh <steph@nieoh.com>
2016-03-14 21:25:27 -0700

    added HTTP verbs

--- elasticAPI/webapp.py
+++ elasticAPI/webapp.py
@@ -1,12 +1,29 @@
 #Run with:
 # gunicorn -k tornado egg:gunicorn #tornado webapp:app
 
-from tornado.web import Application, RequestHandler
+from tornado.web import Application, RequestHandler, HTTPError
 
 class MainHandler(RequestHandler):
 	def get(self):
 		self.write("Hello, world")
 
+class DealsHandler(RequestHandler):
+	def get(self, merchant_name):
+		self.write(merchant_name)
+
+	def post(self, merchant_name):
+		raise HTTPError(403)
+
+	def delete(self, merchant_name):
+		raise HTTPError(403)
+
+	def put(self, merchant_name):
+		raise HTTPError(403)
+
+
+
+
 app = Application([
-	(r"/", MainHandler)
+	(r"/", MainHandler),
+	(r"/v1/(.*)/deals", DealsHandler)
 	])
