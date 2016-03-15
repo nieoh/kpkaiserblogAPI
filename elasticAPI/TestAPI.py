7a6f2c7171ce227836f98ac52dea5269218aa3c9
nieoh <steph@nieoh.com>
2016-03-14 21:25:07 -0700

    testAPI

--- elasticAPI/TestAPI.py
+++ elasticAPI/TestAPI.py
@@ -4,3 +4,31 @@ def test_serverup():
 	r = requests.get('http://localhost:8000')
 
 	assert r.status_code == 200
+	
+
+
+def test_api_get():
+	r = requests.get('http://localhost:8000/v1/homedepot/deals?status=active')
+
+	assert r.status_code == 200
+
+
+
+def test_api_post():
+	r = requests.post('http://localhost:8000/v1/homedepot/deals?status=active')
+
+	assert r.status_code == 403
+
+
+
+def test_api_put():
+	r = requests.put('http://localhost:8000/v1/homedepot/deals?status=active')
+
+	assert r.status_code == 403
+
+
+
+def test_api_delete():
+	r = requests.delete('http://localhost:8000/v1/homedepot/deals?status=active')
+
+	assert r.status_code == 403
